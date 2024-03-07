88eaff20cd9224e8896b71fa5c58b59cc3b82598
Allan Jardine <allan@datatables.net>
2024-03-07 15:47:07 +0000

    Update Bootstrap test for DT2

--- src/js/tests/bootstrapDom.ts
+++ src/js/tests/bootstrapDom.ts
@@ -9,16 +9,20 @@ lib.createTest('`dom` option with Bootstrap', function($) {
 
 	$.fn.dataTable.tables({ api: true }).iterator('table', function(settings, i) {
 		let dom = settings.sDom;
-		let bootstrapTable =
-			$(settings.nTable).hasClass('bootstrap') || $(settings.nTable).hasClass('bootstrap4');
 
-		if (dom.indexOf('<') === -1 && bootstrapTable) {
-			out.push({
-				table: settings.nTable.id,
-				level: 'error',
-				msg:
-					'Your `dom` option appears to be too simple for Bootstrap integration with DataTables! This will result in the table\'s layout looking incorrect. The most common cause for this is using Buttons with a Bootstrap DataTable and using the `B` option in `dom`. <a href="https://datatables.net/extensions/buttons/examples/styling/bootstrap4.html">Please see this example</a> to see how to use Buttons with a Bootstrap DataTable'
-			});
+		// Can be null in DataTables 2
+		if (dom) {
+			let bootstrapTable =
+				$(settings.nTable).hasClass('bootstrap') || $(settings.nTable).hasClass('bootstrap4');
+
+			if (dom.indexOf('<') === -1 && bootstrapTable) {
+				out.push({
+					table: settings.nTable.id,
+					level: 'error',
+					msg:
+						'Your `dom` option appears to be too simple for Bootstrap integration with DataTables! This will result in the table\'s layout looking incorrect. The most common cause for this is using Buttons with a Bootstrap DataTable and using the `B` option in `dom`. <a href="https://datatables.net/extensions/buttons/examples/styling/bootstrap4.html">Please see this example</a> to see how to use Buttons with a Bootstrap DataTable'
+				});
+			}
 		}
 	});
 
